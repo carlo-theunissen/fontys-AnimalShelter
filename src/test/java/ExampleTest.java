41b753793c7612e3bb38d3173aefe080d5ecb76b
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-02-20 14:08:09 +0100

    Fix the first test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class ExampleTest {
     @Test
-    void FirstTest(){
-        Assert.assertFalse(true);
+    public void FirstTest(){
+        Assert.assertFalse(false);
     }
 }
